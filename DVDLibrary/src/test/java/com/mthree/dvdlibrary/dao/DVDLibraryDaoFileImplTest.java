dbc79d7254e53d2156558bc941bc314e02670dfd
Lewi Adrong <lewiadrong@gmail.com>
2022-04-08 11:33:33 -0400

    Added 'testAddGetDVD' test method

--- DVDLibrary/src/test/java/com/mthree/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
+++ DVDLibrary/src/test/java/com/mthree/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
@@ -5,6 +5,8 @@
 package com.mthree.dvdlibrary.dao;
 
 import java.io.FileWriter;
+
+import com.mthree.dvdlibrary.dto.DVD;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,42 +18,76 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author westo
  */
-public class DVDLibraryDaoFileImplTest
-    {
+public class DVDLibraryDaoFileImplTest {
     private DVDLibraryDao testDao;
 
-    public DVDLibraryDaoFileImplTest()
-        {
-        }
+    public DVDLibraryDaoFileImplTest() {
+    }
 
     @BeforeAll
-    public static void setUpClass()
-        {
-        }
+    public static void setUpClass() {
+    }
 
     @AfterAll
-    public static void tearDownClass()
-        {
-        }
+    public static void tearDownClass() {
+    }
 
     @BeforeEach
-    public void setUp() throws Exception
-        {
+    public void setUp() throws Exception {
         String testFile = "testroster.txt";
         // Use the FileWriter to quickly blank the file
         new FileWriter(testFile);
         testDao = new DVDLibraryDaoFileImpl(testFile);
-        }
+    }
 
     @AfterEach
-    public void tearDown()
-        {
-        }
+    public void tearDown() throws Exception {
+    }
 
     @Test
-    public void testSomeMethod()
-        {
-        fail("The test case is a prototype.");
-        }
+    public void testAddGetDVD() throws Exception {
+        // Create our method test inputs
+        String title = "Movie1";
+        DVD dvd = new DVD(title);
+        dvd.setUserRating("5/5");
+        dvd.setStudio("Test");
+        dvd.setRating("PG-13");
+        dvd.setReleaseDate("2022");
+        dvd.setDirectorName("Billy Bob");
+
+        //  Add the student to the DAO
+        testDao.addDVD(dvd);
+        // Get the student from the DAO
+        DVD retrievedStudent = testDao.getDVD(title);
+
+        // Check the data is equal
+        assertEquals(dvd.getTitle(),
+                retrievedStudent.getTitle(),
+                "Checking Movie Title.");
+        assertEquals(dvd.getUserRating(),
+                retrievedStudent.getUserRating(),
+                "Checking Movie User Rating.");
+        assertEquals(dvd.getStudio(),
+                retrievedStudent.getStudio(),
+                "Checking DVD studio.");
+        assertEquals(dvd.getRating(),
+                retrievedStudent.getRating(),
+                "Checking DVD MPAA Rating.");
+        assertEquals(dvd.getReleaseDate(),
+                retrievedStudent.getReleaseDate(),
+                "Checking DVD Release Date.");
+        assertEquals(dvd.getDirectorName(),
+                retrievedStudent.getDirectorName(),
+                "Checking DVD Director's Name.");
+    }
+
+    @Test
+    public void testAddGetAllDVDs() throws Exception {
 
     }
+
+    @Test
+    public void testRemoveDVD() throws  Exception {
+
+    }
+}
